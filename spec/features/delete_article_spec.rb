dee5165cb044b858e7f4efab1904528761cea8d0
Ivan Candido <ivan.candido@powerhrg.com>
2022-06-21 17:32:54 -0400

    Fixed broken delete article spec

--- spec/features/delete_article_spec.rb
+++ spec/features/delete_article_spec.rb
@@ -2,7 +2,9 @@ require "rails_helper"
 
 RSpec.feature "Deleting an Article" do
   before do
-    @article = Article.create(title: "First", body: "Lorem Ipsum")
+    john = User.create!(email: "John@example.com", password: "password")
+    login_as(john)
+    @article = Article.create(title: "First", body: "Lorem Ipsum", user: john)
   end
 
   scenario "User deletes an article" do
